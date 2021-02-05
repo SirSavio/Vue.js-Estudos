c85512c8cbef1219b1018e7d97219c52c1e3b1f0
SirSavio <50343835+SirSavio@users.noreply.github.com>
2021-02-05 09:23:36 -0300

    Adding others projects

---
+++ vue-component-avancado/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="app">
+        <h1>Vue JS</h1>
+        <Post titulo="olaaaa">
+            <template v-slot:cabecalho>
+                <h2>Titulo</h2>
+            </template>
+            <template v-slot>
+                <p>Conteudo</p>
+            </template>
+            <template v-slot:rodape>
+                <small>Rodape</small>
+            </template>
+            <template v-slot:botao>
+              Detalhes
+            </template>
+        </Post>
+
+        <Post titulo="olaaaa">
+            <template v-slot:cabecalho>
+                <h2>Titulo</h2>
+            </template>
+            <template v-slot>
+                <p>Conteudo</p>
+            </template>
+            <template v-slot:rodape>
+                <small>Rodape</small>
+            </template>
+        </Post>
+
+        <Post titulo="olaaaa">
+            <template v-slot:cabecalho>
+                <h2>Titulo</h2>
+            </template>
+            <template v-slot>
+                <p>Conteudo</p>
+            </template>
+            <template v-slot:rodape>
+                <small>Rodape</small>
+            </template>
+        </Post>
+       
+    </div>
+</template>
+
+<script>
+import Post from "./components/Post.vue";
+export default {
+    components: {
+        Post,
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ vue-component-avancado/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.css' // ←
+import '@popperjs/core'
+
+
+createApp(App).mount('#app')
